0ea1483013b9cad1a6e9d053cda398498e266ba7
Jose Diaz <jose.diaz@joedayz.pe>
2025-09-17 20:51:33 -0500

    reordenando testing

--- 05-demo-test-start/test-unit/solutions/expense-restful-service/src/main/java/com/redhat/training/expenses/ExpenseConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.training.expenses;
-
-import java.math.BigDecimal;
-import io.smallrye.config.ConfigMapping;
-
-@ConfigMapping( prefix = "expense" )
-public interface ExpenseConfiguration {
-    BigDecimal maxAmount();
-}
